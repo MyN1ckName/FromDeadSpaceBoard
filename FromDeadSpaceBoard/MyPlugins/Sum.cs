76be7072f98c6f600cefbbfef4d0e902cc49a348
A Egorov on work <mymailegorov@gmail.com>
2020-02-20 18:38:43 +0300

    add Sum class

---
+++ FromDeadSpaceBoard/MyPlugins/Sum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds.test.impl.MyPlugins
+{
+	class Sum : IPlugin
+	{
+		public string PluginName { get { return "Sum"; } }
+		public string Version { get { return "0.0.1"; } }
+
+		public System.Drawing.Image Image
+		{ get { return System.Drawing.Image.FromFile("MyPlugins/img/sum.png"); } }
+
+		public string Description
+		{
+			get { return "Метод Run плагина Sum возвращает сумму двух целых чисел"; }
+		}
+
+		public int Run(int input1, int input2)
+		{
+			int c = input1 + input2;
+			return c;
+		}
+	}
+}
---
+++ FromDeadSpaceBoard/Plugins.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds.test.impl
+{
+	public static class Plugins //: PluginFactory
+	{
+		static List<IPlugin> plugins = new List<IPlugin>();
+		static Plugins()
+		{
+			plugins.Add(new MyPlugins.Sum());
+		}
+
+		public static int PluginsCount { get { return plugins.Count; } }
+
+		static string[] getPluginNames;
+		public static string[] GetPluginNames
+		{
+			get
+			{
+				for (int i = 0; i < plugins.Count - 1; i++)
+				{
+					getPluginNames[i] = plugins[i].PluginName;
+				}
+				return getPluginNames;
+			}
+		}
+
+		public static IPlugin GetPlugin(string pluginName)
+		{
+			var plugin =
+				from item in plugins
+				where item.PluginName.Equals(pluginName)
+				select item;
+
+			return plugin.First();
+		}
+	}
+}
